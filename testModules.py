16fa983c66685ff65e9f62e610bca453e1a69691
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-09-14 16:35:35 +0200

    test for necesary modules

---
+++ testModules.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 16:27:51 2016
+
+@author: natalia
+"""
+
+import csv
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.linalg as scil
+import sys
+
