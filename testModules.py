db80c1753e1438b208fab3bd8390fdf59eb2316d
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-09-15 11:10:30 +0200

    try-except added

--- testModules.py
+++ testModules.py
@@ -5,20 +5,46 @@ Created on Wed Sep 14 16:27:51 2016
 @author: natalia
 """
 from __future__ import print_function
+import sys
 
-import csv
-print('csv:', csv.__version__)
+try:
+    import csv
+    print('csv:', csv.__version__)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    print("Our hint: Please try write in console 'pip install csv'")
+    
+try:
+    import cv2
+    print('cv2:', cv2.__version__)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    print("Our hint: Please try write in console 'pip install opencv'")
 
-import cv2
-print('cv2:', cv2.__version__)
+try:
+    import matplotlib
+    print('matplotlib:', matplotlib.__version__)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    print("Our hint: Please try write in console 'pip install matplotlib'")
 
-import matplotlib
-print('matplotlib:', matplotlib.__version__)
-
-import numpy 
-print('numpy:', numpy.__version__)
-
-import scipy
-print('scipy:', scipy.__version__)
+try:
+    import numpy 
+    print('numpy:', numpy.__version__)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    print("Our hint: Please try write in console 'pip install numpy'")
+    
+try:
+    import scipy
+    print('scipy:', scipy.__version__)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+    print("Our hint: Please try write in console 'pip install scipy'")
 
 
