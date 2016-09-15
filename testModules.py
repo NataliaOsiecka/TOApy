d0746c147064c4202549a2e924116e498eaa7bd1
Natalia Osiecka <nzw.natalia.osiecka@gmail.com>
2016-09-15 10:41:06 +0200

    checking modules version

--- testModules.py
+++ testModules.py
@@ -4,11 +4,21 @@ Created on Wed Sep 14 16:27:51 2016
 
 @author: natalia
 """
+from __future__ import print_function
 
 import csv
+print('csv:', csv.__version__)
+
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-import scipy.linalg as scil
-import sys
+print('cv2:', cv2.__version__)
+
+import matplotlib
+print('matplotlib:', matplotlib.__version__)
+
+import numpy 
+print('numpy:', numpy.__version__)
+
+import scipy
+print('scipy:', scipy.__version__)
+
 
